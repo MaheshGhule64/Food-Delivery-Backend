8521ed06ca956a1b024de85a037840d7e17d7f47
Mahesh Ghule <maheshghule64@gmail.com>
2025-10-08 10:13:34 +0530

    Update foodRouter.js

--- routes/foodRouter.js
+++ routes/foodRouter.js
@@ -7,14 +7,14 @@ const {
   removeFood,
 } = require("../controllers/foodController.js");
 
-// const storage = multer.diskStorage({
-//   destination: "uploads",
-//   filename: (req, file, cb) => {
-//     return cb(null, `${Date.now()}${file.originalname}`);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${file.originalname}`);
+  },
+});
 
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ storage: storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
