436e8224f89dbe6726b25dd4d51a46b3b7cfd94f
Mahesh Ghule <maheshghule64@gmail.com>
2025-10-10 16:09:27 +0530

    Update foodController.js

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -30,7 +30,7 @@ const addFood = async (req, res) => {
     projectId: process.env.GOOGLE_PROJECT_ID,
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replaceAll('mahesh', '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }
   });
 
