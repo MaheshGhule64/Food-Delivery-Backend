553f4925b63d47b3f3f56e97dce51665513e2edd
Mahesh Ghule <maheshghule64@gmail.com>
2025-10-11 10:04:35 +0530

    Create cloud_bucket.js

---
+++ config/cloud_bucket.js
@@ -0,0 +1,14 @@
+const {Storage} = require('@google-cloud/storage')
+
+
+ const storage = new Storage({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }
+  });
+
+const bucket = storage.bucket('delivery-food-images-bucket');
+
+  module.exports = bucket;
