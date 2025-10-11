e5cdd6321459c2274b30ff55dd21ea5fb403c715
Mahesh Ghule <maheshghule64@gmail.com>
2025-10-11 10:11:05 +0530

    Update foodController.js

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -8,7 +8,6 @@ const addFood = async (req, res) => {
 
    try {
     if (!req.file) return res.status(400).json({ error: "No file uploaded" });
-      const bucket = storage.bucket('delivery-food-images-bucket');
     const blob = bucket.file(Date.now() + "-" + req.file.originalname);
 
     const blobStream = blob.createWriteStream({
@@ -47,50 +46,6 @@ const addFood = async (req, res) => {
     console.error(err);
     res.status(500).json({ error: "Server error" });
   }
-
-//    try {
-//     if (!req.file) return res.status(400).send("No file uploaded.");
-
-//     const bucketName = 'food-delivery-food-images'; // replace with your bucket
-//      const tempDir = path.join("/tmp", "uploads");
-//      console.log(tempDir);
-//   const filePath = path.join(tempDir, req.file.originalname);
-//      console.log(filePath);// path to local file
-//   fs.writeFileSync(filePath, req.file.buffer);
-//   const destination = `uploads/${req.file.originalname}`; // path in GCS
-
-//   try {
-//     // Upload file WITHOUT any ACLs (UBLA-safe)
-//     await storage.bucket(bucketName).upload(filePath, {
-//       destination,
-//     });
-
-//     console.log(`✅ File uploaded to gs://${bucketName}/${destination}`);
-//     const image_url = `https://storage.cloud.google.com/${bucketName}/uploads/${req.file.originalname}`;
-  //   const food = new foodModel({
-  //   name: req.body.name,
-  //   description: req.body.description,
-  //   price: req.body.price,
-  //   category: req.body.category,
-  //   image: image_url,
-  // }); 
-  
-  // try {
-  //   await food.save();
-  //   res.json({ success: true, message: "Food Added" });
-  // } catch (err) {
-  //   console.log(err);
-  //   res.json({ success: false, message: "Error" });
-  // } 
-
-//    } catch (error) {
-//     console.error('❌ Upload failed:', error.message);
-//   }
-  
-// }catch(err){
-//   console.log(err);
-//   res.status(500).send(err);
-// }
 };
 
 // get food items
@@ -109,8 +64,12 @@ const listFood = async (req, res) => {
 const removeFood = async (req, res) => {
   try {
     const food = await foodModel.findByIdAndDelete(req.body.id);
-    fs.unlink(`tmp/${food.image}`, () => {});
-    res.json({ success: true, message: "Food Removed" });
+  
+    const image = food.image;
+    const startpos = image.lastIndexOf('/');
+    const filename = image.substring(startpos+1);   
+
+    await bucket.file(filename).delete();
   } catch (error) {
     res.json({ success: false, message: "Error" });
   }
